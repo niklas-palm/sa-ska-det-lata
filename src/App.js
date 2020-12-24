db73a0573bc2216d808ee65fd9016232f6c799ff
robot <robot@example.com>
2020-12-24 12:49:17 +0100

    Update App logic to handle click

--- src/App.js
+++ src/App.js
@@ -1,34 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./styling/app.scss";
 
 function App() {
-  const song = {
+  const [song, setSong] = useState({
     "it's": {
       clicked: false,
+      red: false,
     },
     the: {
       clicked: false,
+      red: true,
     },
     most: {
       clicked: false,
+      red: false,
     },
     wonderful: {
       clicked: false,
+      red: false,
     },
     time: {
-      clicked: false,
+      clicked: true,
+      red: false,
     },
-  };
+  });
 
   const renderGameTiles = () => {
-    return Object.keys(song).map((word) => {
+    return Object.keys(song).map((word, index) => {
       return (
-        <div key={word} className="GameTile">
-          {word}
+        <div
+          onClick={() => onClick(word)}
+          key={word}
+          className="GameTile"
+          style={{
+            backgroundColor:
+              song[word].clicked && song[word].red ? "red" : "#1ca9f0",
+          }}
+        >
+          {song[word].clicked ? word : index + 1}
         </div>
       );
     });
+  };
+
+  const onClick = (word) => {
+    console.log(word);
+    setSong((prev) => ({
+      ...prev,
+      [word]: {
+        ...song[word],
+        clicked: !song[word].clicked,
+      },
+    }));
   };
 
   return (
