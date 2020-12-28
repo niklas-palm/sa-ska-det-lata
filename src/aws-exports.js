b1b353e1205b9c0350a4335e26fe50a15aaad123
robot <robot@example.com>
2020-12-28 11:34:52 +0100

    Aparently aws-export doesnt contain sensive info, so removed it from .gitignore to make CD-pipeline a lot easier

---
+++ src/aws-exports.js
@@ -0,0 +1,11 @@
+const awsmobile = {
+  aws_project_region: "eu-north-1",
+  aws_cognito_identity_pool_id:
+    "eu-north-1:163b2922-bae3-46fd-9d2a-f24938c475b8",
+  aws_cognito_region: "eu-north-1",
+  aws_user_pools_id: "eu-north-1_yphTeMALz",
+  aws_user_pools_web_client_id: "1hn7uqo16qnbf0tbv8g6bsjuid",
+  oauth: {},
+};
+
+export default awsmobile;
