0c72fbe44cd884b78deae874377b9b2ce6eee5c5
robot <robot@example.com>
2020-12-29 13:36:27 +0100

    Add styling to landing page + buttons

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -1,18 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import Header from "../components/Header";
 
 import "../styling/landing.scss";
 
 const Landing = () => {
+  const history = useHistory();
+
+  const navigateTo = (path) => {
+    history.push(path);
+  };
+
   return (
     <div className="LandingContainer">
       <Header />
       <div className="LandingContent">
-        <Link to="/CreateGame">Create Game</Link>
+        <div
+          onClick={() => navigateTo("/CreateGame")}
+          className="LandingButton"
+        >
+          Create new game
+        </div>
+        <div onClick={() => navigateTo("/MyGames")} className="LandingButton">
+          Play a game
+        </div>
+        <div
+          onClick={() => navigateTo("/Instructions")}
+          className="LandingButton"
+        >
+          How to play?
+        </div>
+        {/* <Link to="/CreateGame">Create Game</Link>
         <Link to="/MyGames">MyGames</Link>
-        <Link to="/Instructions">How to play</Link>
+        <Link to="/Instructions">How to play</Link> */}
       </div>
     </div>
   );
